14331bd48cef880524cbe8599bf3cae1a9270c2d
sam su <36393679+samyilin@users.noreply.github.com>
2026-09-06 23:45:15 -0700

    Use mini pick text items correctly

--- lua/dbtpal/picker_mini.lua
+++ lua/dbtpal/picker_mini.lua
@@ -8,13 +8,12 @@ function M.setup(picker)
             mini.start {
                 source = {
                     name = opts.prompt or "Select",
-                    items = opts.items or {},
-                    show = function(index)
-                        local item = (opts.items or {})[index]
-                        if opts.format_item then return opts.format_item(item) end
-                        return item.name or tostring(item)
-                    end,
-                    choose = function(index) callback((opts.items or {})[index]) end,
+                    items = vim.tbl_map(function(item)
+                        local value = vim.deepcopy(item)
+                        value.text = opts.format_item and opts.format_item(item) or item.name or tostring(item)
+                        return value
+                    end, opts.items or {}),
+                    choose = function(item) callback(item) end,
                 },
             }
         end,
