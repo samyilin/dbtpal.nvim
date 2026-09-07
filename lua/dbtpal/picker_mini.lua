24452ddb0e73a154507a9efa5562ec461db7831c
sam su <36393679+samyilin@users.noreply.github.com>
2026-09-06 23:21:20 -0700

    Format resource names in mini pick

--- lua/dbtpal/picker_mini.lua
+++ lua/dbtpal/picker_mini.lua
@@ -9,6 +9,10 @@ function M.setup(picker)
                 source = {
                     name = opts.prompt or "Select",
                     items = opts.items or {},
+                    show = function(item)
+                        if opts.format_item then return opts.format_item(item) end
+                        return item.name or tostring(item)
+                    end,
                     choose = function(item) callback(item) end,
                 },
             }
